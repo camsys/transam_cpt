6531211bf31b1e18b767da1d74499e1d388c3f25
Julian Ray <jray@camsys.com>
2014-04-25 15:17:29 -0400

    Add a helper and pluggable main nav

---
+++ app/helpers/transam_cpt_helper.rb
@@ -0,0 +1,11 @@
+module TransamHelper
+
+  # Return the version of TransAM CPT that is running
+  def transam_cpt_version
+    begin
+      Gem.loaded_specs['transam_cpt'].version
+    rescue
+    end
+  end
+
+end
