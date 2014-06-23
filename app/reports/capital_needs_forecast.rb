569d405d54d02e1fb02d296020ac57393791b4df
Julian Ray <jray@camsys.com>
2014-06-23 07:32:43 -0400

    Refactor capital needs forcast report to remove duplicated logic

--- app/reports/capital_needs_forecast.rb
+++ app/reports/capital_needs_forecast.rb
@@ -32,24 +32,9 @@ class CapitalNeedsForecast < AbstractReport
   
   def get_data(organization_id_list, params)
     
-    # Capital Needs by year
-    analysis_year = current_fiscal_year_year
-    last_year = analysis_year + MAX_FORECASTING_YEARS
-
-    a = []
-    labels = ['Fiscal Year', 'State', 'Federal', 'Local']
-        
-    (analysis_year..last_year).each do |year|
-      report_row = CptReportRow.new(year)
-      # get the capital projects for this analysis year
-      capital_projects =  CapitalProject.where('capital_projects.organization_id IN (?) AND capital_projects.fy_year = ?', organization_id_list, year)
-      capital_projects.find_each do |cp|
-        report_row.add(cp)
-      end
-      a << [fiscal_year(year), report_row.state_request, report_row.federal_request, report_row.local_request]
-    end
+    capital_projects = CapitalProject.where('capital_projects.organization_id IN (?)', organization_id_list)
+    return get_data_from_result_list(capital_projects)
     
-    return {:labels => labels, :data => a}      
   end
   
 end
