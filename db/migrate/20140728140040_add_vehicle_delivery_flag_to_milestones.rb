e5ff24b93cbbd024be9212130cbe422e55c0832f
Julian Ray <jray@camsys.com>
2014-08-26 09:48:30 -0400

    More migrations to throw out

--- db/migrate/20140728140040_add_vehicle_delivery_flag_to_milestones.rb
+++
@@ -1,5 +0,0 @@
-class AddVehicleDeliveryFlagToMilestones < ActiveRecord::Migration
-  def change
-    add_column  :milestone_types, :is_vehicle_delivery, :boolean, :after => :description
-  end
-end
