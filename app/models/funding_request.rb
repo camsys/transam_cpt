5c86da5026b21ef195d4850f1aede0879331780c
Julian Ray <jray@camsys.com>
2014-08-28 10:01:40 -0400

    Don't require state or federal funds when creating a funding request [fixes #77758340]

--- app/models/funding_request.rb
+++ app/models/funding_request.rb
@@ -52,8 +52,8 @@ class FundingRequest < ActiveRecord::Base
   # Validations
   #------------------------------------------------------------------------------
   validates :object_key,                        :presence => :true, :uniqueness => :true
-  validates :federal_funding_line_item_id,      :presence => :true
-  validates :state_funding_line_item_id,        :presence => :true
+  #validates :federal_funding_line_item_id,      :presence => :true
+  #validates :state_funding_line_item_id,        :presence => :true
   validates :activity_line_item_id,             :presence => :true
   validates :federal_amount,                    :numericality => {:only_integer => :true, :greater_than_or_equal_to => 0}, :allow_nil => true
   validates :state_amount,                      :numericality => {:only_integer => :true, :greater_than_or_equal_to => 0}, :allow_nil => true
