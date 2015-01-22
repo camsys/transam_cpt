95604c6d03a200686b78ff7f4e3704d1f4f1847c
Julian Ray <jray@camsys.com>
2015-01-22 13:00:07 -0500

    Fix issues with CP workflow actions

--- app/helpers/transam_cpt_helper.rb
+++ app/helpers/transam_cpt_helper.rb
@@ -8,13 +8,29 @@ module TransamCptHelper
     end
   end
 
+  # Returns the correct icon for a workflow asset
+  def get_event_icon(event_name)
+
+    if event_name == 'retract'
+      'fa-reply'
+    elsif event_name == 'submit'
+      'fa-share'
+    elsif event_name == 'approve'
+      'fa-plus-square'
+    elsif event_name == 'return'
+      'fa-chevron-circle-left'
+    else
+      ''
+    end
+  end
+
   # Returns the correct cost for a swimlane/asset/year
   def get_swimlane_activity_cost(asset, year)
 
     if asset.scheduled_disposition_year == year
       # nothing to display
       return nil
-    elsif asset.scheduled_replacement_year == year 
+    elsif asset.scheduled_replacement_year == year
       cost = asset.scheduled_replacement_cost.present? ? asset.scheduled_replacement_cost : asset.policy_rule.replacement_cost
     elsif asset.scheduled_rehabilitation_year == year
       cost = asset.scheduled_rehabilitation_cost.present? ? asset.scheduled_rehabilitation_cost : asset.policy_rule.rehabilitation_cost
@@ -23,30 +39,30 @@ module TransamCptHelper
     end
     format_as_currency(cost)
   end
-  
+
   # Returns the correct swimlane icon color for an asset
   def get_swimlane_icon(asset, year)
-    
+
     if asset.scheduled_disposition_year == year
-      'fa-times-circle'      
+      'fa-times-circle'
     elsif asset.scheduled_rehabilitation_year == year
-      'fa-wrench'            
-    elsif asset.scheduled_replacement_year == year 
+      'fa-wrench'
+    elsif asset.scheduled_replacement_year == year
       'fa-refresh'
     else
       'fa-warning'
     end
   end
-  
+
   # Returns the correct swimlane panel color for an asset
   def get_swimlane_class(asset, year)
     if asset.in_backlog?
       'panel-default'
-    elsif asset.scheduled_replacement_year and asset.scheduled_replacement_year < asset.policy_replacement_year 
+    elsif asset.scheduled_replacement_year and asset.scheduled_replacement_year < asset.policy_replacement_year
       'panel-warning'
     else
       'panel-info'
     end
   end
-  
+
 end
