c93618e02d0b84e476185790a4bed8120ce75d2a
Julian Ray <jray@camsys.com>
2015-05-28 23:38:33 -0400

    Update date picker date representation and allow for passing in nulls

--- app/controllers/activity_line_items_controller.rb
+++ app/controllers/activity_line_items_controller.rb
@@ -216,7 +216,7 @@ class ActivityLineItemsController < OrganizationAwareController
       params[:activity_line_item][:milestones_attributes].each do |milestone_hash|
         #puts milestone_hash.inspect
         date_str = milestone_hash[1]['milestone_date']
-        form_date = Date.strptime(date_str, '%m-%d-%Y')
+        form_date = Date.strptime(date_str, '%m/%d/%Y')
         milestone_hash[1]['milestone_date'] = form_date.strftime('%Y-%m-%d')
       end
 
