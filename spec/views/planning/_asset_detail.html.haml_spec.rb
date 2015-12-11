b7814a98dfb6e96e57aa8e7df3d9ae4137ffe8ba
Lydia Chang <mathmerized@gmail.com>
2015-12-11 14:40:00 -0500

    fix issue with asset type and subtype in test

--- spec/views/planning/_asset_detail.html.haml_spec.rb
+++ spec/views/planning/_asset_detail.html.haml_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe "planning/_asset_detail.html.haml", :type => :view do
   it 'asset' do
-    test_asset = create(:buslike_asset, :estimated_condition_type_id => 6, :asset_type => create(:asset_type), :asset_subtype => create(:asset_subtype))
+    if AssetType.count == 0 || AssetSubtype.count == 0
+      create(:asset_type)
+      create(:asset_subtype)
+    end
+    test_asset = create(:buslike_asset, :estimated_condition_type_id => 6)
     render 'planning/asset_detail', :asset => test_asset, :year => Date.today.year
 
     expect(rendered).to have_content(test_asset.asset_subtype.to_s.upcase)
