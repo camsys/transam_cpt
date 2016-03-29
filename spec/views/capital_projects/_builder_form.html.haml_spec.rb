6a808b16e20c6d251d8f4ac8b9291ecaba70d517
Lydia Chang <mathmerized@gmail.com>
2016-03-29 13:21:54 -0400

    fix syntax error preventing multiple org filter

--- spec/views/capital_projects/_builder_form.html.haml_spec.rb
+++ spec/views/capital_projects/_builder_form.html.haml_spec.rb
@@ -6,8 +6,10 @@ describe "capital_projects/_builder_form.html.haml", :type => :view do
     assign(:asset_types, AssetType.all)
     assign(:fiscal_years, [2010])
     assign(:organization, create(:organization))
+    assign(:organization_list,[1,2])
     render
 
+    expect(rendered).to have_field('builder_proxy_organization_id')
     expect(rendered).to have_xpath('//input[@name="builder_proxy[asset_types][]"]')
     expect(rendered).to have_field('builder_proxy_start_fy')
   end
