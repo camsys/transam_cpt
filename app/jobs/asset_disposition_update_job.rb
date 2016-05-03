623c34094e920ff3517bf8863bc3b568bd00ce32
mmaranda-cs <mmaranda@camsys.com>
2016-05-03 08:45:59 -0400

    [#116538279] Updating to asset_disposition_update_job to only transfer an asset if the event is a transfer disposition.

--- app/jobs/asset_disposition_update_job.rb
+++ app/jobs/asset_disposition_update_job.rb
@@ -20,7 +20,7 @@ class AssetDispositionUpdateJob < AbstractAssetUpdateJob
 
     asset_event_type = AssetEventType.where(:class_name => 'DispositionUpdateEvent').first
     asset_event = AssetEvent.where(:asset_id => asset.id, :asset_event_type_id => asset_event_type.id).last
-    if(asset_event.dispostion_type.name == 'Trasnferred')
+    if(asset_event.disposition_type_id == 2)
       asset.transfer asset_event.organization_id
     end
 
