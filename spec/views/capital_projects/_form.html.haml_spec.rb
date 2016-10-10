95cb985daa60ed81fe4464fd5fb0f8da539f9ed6
Lydia Chang <mathmerized@gmail.com>
2016-10-10 15:00:14 -0400

    update form view test for setting capital project org if user has many orgs

--- spec/views/capital_projects/_form.html.haml_spec.rb
+++ spec/views/capital_projects/_form.html.haml_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe "capital_projects/_form.html.haml", :type => :view do
   it 'fields' do
     assign(:project, CapitalProject.new)
+    assign(:organization_list, [1,2])
     render
 
+    expect(rendered).to have_field('capital_project_organization_id')
     expect(rendered).to have_field('capital_project_title')
     expect(rendered).to have_field('capital_project_fy_year')
     expect(rendered).to have_field('capital_project_team_ali_code_id')
