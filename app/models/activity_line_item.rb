79c5730790c84ff96c4e9806ac50b18ee2b35b64
Lydia Chang <mathmerized@gmail.com>
2016-12-30 12:49:55 -0500

    funding tagging data structures

--- app/models/activity_line_item.rb
+++ app/models/activity_line_item.rb
@@ -54,13 +54,6 @@ class ActivityLineItem < ActiveRecord::Base
   # Use a nested form to set the milestones
   accepts_nested_attributes_for :milestones, :allow_destroy => true
 
-  # Has 0 or more funding plans -- A funding plan identifies the sources and amounts
-  # of funds that will be used to fund the ALI
-  has_many    :funding_plans,     :dependent => :destroy
-
-  # Has 0 or more funding requests, These will be removed if the project is removed.
-  #has_many    :funding_requests,  :dependent => :destroy
-
   # Has 0 or more comments. Using a polynmorphic association
   has_many    :comments,  :as => :commentable
 
@@ -145,48 +138,6 @@ class ActivityLineItem < ActiveRecord::Base
 
   def category_team_ali_code
     team_ali_code_id.present? ? team_ali_code.parent.code : ''
-  end
-
-  # Returns the total amount of funding planned for this ali
-  def total_funds
-    funding_plans.sum(:amount)
-  end
-
-  def funds_required
-    cost - total_funds
-  end
-
-  # Returns the total value of federal funds requested
-  def federal_funds
-    0
-
-    # TODO: re-enable following line when funding_source is enabled
-    #funding_plans.joins(:funding_source).sum("amount * (funding_sources.federal_match_required / 100.0)")
-  end
-
-  # Returns the total value of state funds requested
-  def state_funds
-    0
-
-    # TODO: re-enable following line when funding_source is enabled
-    #funding_plans.joins(:funding_source).sum("amount * (funding_sources.state_match_required / 100.0)")
-  end
-
-  # Returns the total value of local funds requested
-  def local_funds
-    0
-
-    # TODO: re-enable following line when funding_source is enabled
-    #funding_plans.joins(:funding_source).sum("amount * (funding_sources.local_match_required / 100.0)")
-  end
-  def federal_percentage
-    100.0 * (federal_funds / total_funds) if total_funds.to_i > 0
-  end
-  def state_percentage
-    100.0 * (state_funds / total_funds) if total_funds.to_i > 0
-  end
-  def local_percentage
-    100.0 * (local_funds / total_funds) if total_funds.to_i > 0
   end
 
   # Returns the cost difference between the anticpated cost by the user and the cost estimated
