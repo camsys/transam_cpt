6f3640c22b15688fafa8438df3bb17443635ca1d
Lydia Chang <mathmerized@gmail.com>
2017-03-10 15:45:32 -0500

    update tests for reverted header menu

--- spec/views/shared/_cpt_policy_sub_nav.html.haml_spec.rb
+++ spec/views/shared/_cpt_policy_sub_nav.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "shared/_cpt_policy_sub_nav_second_column.html.haml", :type => :view do
+describe "shared/_cpt_policy_sub_nav.html.haml", :type => :view do
   it 'menu' do
     test_user = create(:admin)
     allow(controller).to receive(:current_user).and_return(test_user)
