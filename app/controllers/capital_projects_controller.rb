fd1190f49c68d6030cded372a8d97e44f61ba0e1
Lydia Chang <mathmerized@gmail.com>
2017-05-03 08:56:24 -0400

    [fixes#144721507] [fixes#144774457] fix project sums and fix displaying of asset subtypes on ALI filter detail pg

--- app/controllers/capital_projects_controller.rb
+++ app/controllers/capital_projects_controller.rb
@@ -135,6 +135,8 @@ class CapitalProjectsController < AbstractCapitalProjectsController
         report_instance = @report.class_name.constantize.new
         @data = report_instance.get_data_from_result_list(@projects)
 
+        @alis = ActivityLineItem.where(capital_project_id: @projects.ids).uniq # override @alis to properly sum costs of capital projects
+        Rails.logger.info "blueberry pie"
         @total_projects_cost_by_year =@alis.group("activity_line_items.fy_year").sum(ActivityLineItem::COST_SUM_SQL_CLAUSE)
         @total_projects_cost = @total_projects_cost_by_year.map { |k,v| v}.sum
         #@total_projects_state_funds = @projects.total_state_funds
