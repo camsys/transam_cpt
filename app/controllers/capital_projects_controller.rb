76d45277719923fafd31b935a831339c0c74f4c8
mmaranda-cs <mmaranda@camsys.com>
2017-05-16 15:02:08 -0400

    [#145275067] code cleanup

--- app/controllers/capital_projects_controller.rb
+++ app/controllers/capital_projects_controller.rb
@@ -74,15 +74,14 @@ class CapitalProjectsController < AbstractCapitalProjectsController
 
       @has_locked_sogr_this_fiscal_year = []
 
-      # unless (current_user.organization.organization_type.class_name == "Grantor")
+      unless (current_user.organization.organization_type.class_name == "Grantor")
         #TODO Makes this a useful join
-        capital_plan_modules = CapitalPlanModule.where(id: CapitalPlanModuleType.find_by(class_name: 'UnconstrainedCapitalPlanModule').id, capital_plan_id: CapitalPlan.where(organization_id: @organization_list, fy_year: current_fiscal_year_year+1)).where.not(completed_at: nil)
+        capital_plan_modules = CapitalPlanModule.where(id: CapitalPlanModuleType.find_by(class_name: 'UnconstrainedCapitalPlanModule').id, capital_plan_id: CapitalPlan.where(organization_id: @organization_list, fy_year: current_planning_year_year)).where.not(completed_at: nil)
 
         capital_plan_modules.each { |cpm|
           @has_locked_sogr_this_fiscal_year << cpm.capital_plan.organization.id.to_s
         }
-      # end
-
+      end
     end
 
     @message = "Creating SOGR capital projects. This process might take a while."
