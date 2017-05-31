c91093d9caaf81e8584dae5ef07b51cd04df9ed3
Lydia Chang <mathmerized@gmail.com>
2017-05-31 11:27:33 -0400

    [fixes#146085203] cleanup complete action of constrained plan module

--- app/capital_plan_actions/constrained_capital_plan_module.rb
+++ app/capital_plan_actions/constrained_capital_plan_module.rb
@@ -10,13 +10,8 @@ class ConstrainedCapitalPlanModule < BaseCapitalPlanModule
 
   def set_ali_is_complete_status is_complete_status
     #     Get all ALI associeated with the organizaton
-    alis = ActivityLineItem.where(organization_id: @capital_plan_module.capital_plan.organization_id)
-
     #   Mark all the ALI is planning complete
     #   Save the ALIs
-    alis.each_char { |ali|
-      ali.is_planning_complete = is_complete_status
-      ali.save
-    }
+    ActivityLineItem.where(organization_id: @capital_plan_module.capital_plan.organization_id).update_all(is_planning_complete: is_complete_status)
   end
 end
