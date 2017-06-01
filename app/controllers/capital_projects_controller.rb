044a5585d57e8e30f2be3ee040511d5f1245a989
Lydia Chang <mathmerized@gmail.com>
2017-06-01 10:50:54 -0400

    grantor can build at any year

--- app/controllers/capital_projects_controller.rb
+++ app/controllers/capital_projects_controller.rb
@@ -63,10 +63,12 @@ class CapitalProjectsController < AbstractCapitalProjectsController
     @fiscal_years = get_fiscal_years
     @builder_proxy = BuilderProxy.new
 
-    @has_locked_sogr_this_fiscal_year = CapitalPlanModule.joins(:capital_plan_module_type, :capital_plan).where(capital_plan_module_types: {name: ['Unconstrained Plan', 'Constrained Plan']}, capital_plans: {organization_id: @organization_list, fy_year: current_planning_year_year}).where('capital_plan_modules.completed_at IS NOT NULL').pluck('capital_plans.organization_id')
+    unless (current_user.organization.organization_type.class_name == "Grantor")
+      @has_locked_sogr_this_fiscal_year = CapitalPlanModule.joins(:capital_plan_module_type, :capital_plan).where(capital_plan_module_types: {name: ['Unconstrained Plan', 'Constrained Plan']}, capital_plans: {organization_id: @organization_list, fy_year: current_planning_year_year}).where('capital_plan_modules.completed_at IS NOT NULL').pluck('capital_plans.organization_id')
+    end
 
     if @organization_list.count == 1
-      if @has_locked_sogr_this_fiscal_year.include? @organization_list.first
+      if @has_locked_sogr_this_fiscal_year && (@has_locked_sogr_this_fiscal_year.include? @organization_list.first)
         @fiscal_years = @fiscal_years[1..-1]
       end
       if Organization.get_typed_organization(Organization.find_by(id: @organization_list.first)).has_sogr_projects?
