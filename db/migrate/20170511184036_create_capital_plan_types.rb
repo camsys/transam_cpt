75d5633fe3870c5ad389fa04cf6df3c2b4c840de
Lydia Chang <mathmerized@gmail.com>
2017-06-06 12:08:17 -0400

    properly separate funding portions of capital plans into funding engine

--- db/migrate/20170511184036_create_capital_plan_types.rb
+++ db/migrate/20170511184036_create_capital_plan_types.rb
@@ -7,18 +7,5 @@ class CreateCapitalPlanTypes < ActiveRecord::Migration
     end
 
     add_column :organizations, :capital_plan_type_id, :integer
-
-    unless Rails.env == 'test'
-      capital_plan_types = [
-          {name: 'Transit Capital Plan', description: 'Transit Capital Plan', active: true}
-      ]
-
-
-      capital_plan_types.each do |type|
-        CapitalPlanType.create!(type)
-      end
-
-      Organization.update_all(capital_plan_type_id: 1)
-    end
   end
 end
