9465b0975fc3c64e206712427d6b877f6e997d52
Lydia Chang <mathmerized@gmail.com>
2017-06-09 08:38:07 -0400

    [#145005319] csv export for reports that use Presenter classes and UI formatting for reporting actions

--- app/models/capital_project_report_presenter.rb
+++ app/models/capital_project_report_presenter.rb
@@ -6,6 +6,8 @@ class CapitalProjectReportPresenter
   attr_accessor :sogr_flag
   attr_accessor :emergency_flag
 
+  include TransamFormatHelper
+
   def organization_ids
     if projects.blank?
       []
@@ -18,4 +20,40 @@ class CapitalProjectReportPresenter
   def projects_by_organization
     @projects_by_organization ||= @projects.includes(:organization).group_by(&:organization)
   end
+
+  def[](index)
+    case index.to_s
+      when 'labels'
+        ['Org', 'FY', 'Project', 'Title', 'Scope', 'Cost', '# ALIs', '# Assets', 'Type', 'Emgcy', 'SOGR', 'Shadow', 'Multi Year']
+      when 'data'
+        data = []
+        projects_by_organization.each do |org, projects|
+          projects.each do |p|
+            count_assets = 0
+            p.activity_line_items.each{|x| count_assets += x.assets.count}
+            row = [org.short_name]
+            row << format_as_fiscal_year(p.fy_year)
+            row << p.project_number
+            row << p.title
+            row << p.team_ali_code.scope
+            row << p.total_cost
+            row << p.activity_line_items.count
+            row << count_assets
+            row << p.capital_project_type.code
+            row << (p.emergency? ? 'Y' : '')
+            row << (p.sogr? ? 'Y' : '')
+            row << (p.notional? ? 'Y' : '')
+            row << (p.multi_year? ? 'Y' : '')
+            data << row
+          end
+        end
+
+        data
+      when 'formats'
+        [nil, nil, nil, nil, nil, :currency, nil, nil, nil, nil, nil, nil, nil]
+      else
+        []
+    end
+  end
+
 end
