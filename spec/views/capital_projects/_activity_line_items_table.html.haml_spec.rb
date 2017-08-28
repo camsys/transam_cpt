0a1ad4d359ab7eb2439a3384df252072061c75f7
mathmerized <mathmerized@gmail.com>
2017-08-28 19:33:43 -0400

    test

--- spec/views/capital_projects/_activity_line_items_table.html.haml_spec.rb
+++ spec/views/capital_projects/_activity_line_items_table.html.haml_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe "capital_projects/_activity_line_items_table.html.haml", :type => :view do
   it 'list' do
+    allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
     test_proj = create(:capital_project, :fy_year => 2014)
     test_ali = create(:activity_line_item, :capital_project => test_proj)
     render 'capital_projects/activity_line_items_table', :project => test_proj
