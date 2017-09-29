962ec47d5783d2c3f997112a0ef8674bf48cda51
mathmerized <mathmerized@gmail.com>
2017-09-29 12:04:05 -0400

    [fixes#151523215] bug in checking for not pinned ALI in SOGR builder

--- app/services/capital_project_builder.rb
+++ app/services/capital_project_builder.rb
@@ -539,13 +539,12 @@ class CapitalProjectBuilder
 
 
     if asset.present?
+      not_pinned_alis = ActivityLineItem.distinct.joins(:assets).where('assets.replacement_status_type_id != 4 OR replacement_status_type_id IS NULL')
       if asset.fuel_type_id.present?
-        ali = ActivityLineItem.find_by('capital_project_id = ? AND team_ali_code_id = ? AND fuel_type_id = ?', project.id, ali_code.id, (fuel_type_id || asset.fuel_type_id))
+        ali = not_pinned_alis.find_by('capital_project_id = ? AND team_ali_code_id = ? AND fuel_type_id = ?', project.id, ali_code.id, (fuel_type_id || asset.fuel_type_id))
       else
-        ali = ActivityLineItem.find_by('capital_project_id = ? AND team_ali_code_id = ?', project.id, ali_code.id)
+        ali = not_pinned_alis.find_by('capital_project_id = ? AND team_ali_code_id = ?', project.id, ali_code.id)
       end
-      # ali cant be a "pinned" ALI
-      ali = nil if ali.try(:pinned?)
 
       # if there is an exisiting ALI, see if the asset is in it
       if ali
