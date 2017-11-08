303841083e34d2ba2b2139091d31dafe5fa1cc90
mathmerized <mathmerized@gmail.com>
2017-11-08 08:39:29 -0500

    try different way to load ali filters

--- db/seeds.rb
+++ db/seeds.rb
@@ -183,5 +183,4 @@ data.each do |row|
   x.save!
 end
 
-Rake.application.rake_require '../lib/tasks/transam'
-Rake::Task['transam:add_user_activity_line_item_filters'].invoke
+load('system_ali_filters_loader.rb')
