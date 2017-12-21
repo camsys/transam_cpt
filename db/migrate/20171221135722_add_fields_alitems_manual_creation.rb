ba421b2665934816ad67aba81b8c335c4363a2ef
mmaranda-cs <mmaranda@camsys.com>
2017-12-21 16:07:40 -0500

    [#153717698] Adding the columns required for the new options on manually added ALIs

---
+++ db/migrate/20171221135722_add_fields_alitems_manual_creation.rb
@@ -0,0 +1,7 @@
+class AddFieldsAlitemsManualCreation < ActiveRecord::Migration
+  def change
+    add_column :activity_line_items, :purchased_new, :boolean, before: :active
+    add_column :activity_line_items, :count, :integer, after: :purchased_new
+    add_column :activity_line_items, :length, :integer, after: :count
+  end
+end
