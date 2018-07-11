1eb253f1a85504040a8e8cd39e02138461b6cc2c
mathmerized <mathmerized@gmail.com>
2018-07-11 11:27:33 -0400

    cleanup seeds to match data migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,15 +53,9 @@ replacement_status_types = [
 system_config_extensions = [
     {class_name: 'CapitalProject', extension_name: 'TransamKeywordSearchable', active: true},
     {class_name: 'ActivityLineItem', extension_name: 'TransamKeywordSearchable', active: true},
-    {class_name: 'RevenueVehicle', extension_name: 'TransamPlannable', active: true},
-    {class_name: 'ServiceVehicle', extension_name: 'TransamPlannable', active: true},
-    {class_name: 'CapitalEquipment', extension_name: 'TransamPlannable', active: true},
-    {class_name: 'FacilityComponent', extension_name: 'TransamPlannable', active: true},
-    {class_name: 'Facility', extension_name: 'TransamPlannable', active: true},
+    {class_name: 'TransamAsset', extension_name: 'TransamCapitalPlannable', active: true},
     {class_name: 'TransitOperator', extension_name: 'TransamPlanningOrganization', active: true},
     {class_name: 'Grantor', extension_name: 'TransamPlanningOrganization', active: true},
-    {class_name: 'CapitalProject', extension_name: 'TransamFundableCapitalProject', active: true},
-    {class_name: 'ActivityLineItem', extension_name: 'TransamFundable', active: true},
     {class_name: 'User', extension_name: 'TransamPlanningFilters', active: true}
 
 ]
