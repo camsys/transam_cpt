07ab957347789b450d838b6d65ba17e472f12ff0
mathmerized <mathmerized@gmail.com>
2018-07-27 10:28:37 -0400

    cleanup data loading TTPLAT-234

--- db/data_migrations/20180719152435_move_activity_line_items_transam_assets.rb
+++ db/data_migrations/20180719152435_move_activity_line_items_transam_assets.rb
@@ -1,7 +1,7 @@
 class MoveActivityLineItemsTransamAssets < ActiveRecord::DataMigration
   def up
     ActivityLineItemsAsset.where(transam_asset_id: nil).where.not(asset_id: nil).each do |ali_asset|
-      ali_asset.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: ali_asset.asset_id).transam_asset.id)
+      ali_asset.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: ali_asset.asset_id).try(:transam_asset).try(:id))
     end
   end
 end
