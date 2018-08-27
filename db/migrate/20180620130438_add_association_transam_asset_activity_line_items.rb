26b8bd91377b3db20ff57002f2421b2e5d8e2a1f
mathmerized <mathmerized@gmail.com>
2018-08-27 08:04:45 -0400

    TTPLAT-358 add indices to transam_asset reference columns

--- db/migrate/20180620130438_add_association_transam_asset_activity_line_items.rb
+++ db/migrate/20180620130438_add_association_transam_asset_activity_line_items.rb
@@ -1,5 +1,5 @@
 class AddAssociationTransamAssetActivityLineItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :activity_line_items_assets, :transam_asset_id, :integer, after: :asset_id
+    add_reference :activity_line_items_assets, :transam_asset, after: :asset_id
   end
 end
