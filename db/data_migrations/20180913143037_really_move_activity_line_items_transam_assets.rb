495c37a85937cc21e1a853faab928bd2704be689
mathmerized <mathmerized@gmail.com>
2018-09-13 10:32:28 -0400

    move data load scripts to the end because must be after intial data load in transit engine

--- db/data_migrations/20180913143037_really_move_activity_line_items_transam_assets.rb
+++ db/data_migrations/20180913143037_really_move_activity_line_items_transam_assets.rb
@@ -1,4 +1,4 @@
-class MoveActivityLineItemsTransamAssets < ActiveRecord::DataMigration
+class ReallyMoveActivityLineItemsTransamAssets < ActiveRecord::DataMigration
   def up
     ActivityLineItemsAsset.all.each do |ali_asset|
       ali_asset.update_columns(transam_asset_id: TransitAsset.find_by(asset_id: ali_asset.asset_id).try(:transam_asset).try(:id))
