e94e1ef9e9a259d02a1ae63c597b05487cc908b9
mathmerized <mathmerized@gmail.com>
2018-12-11 15:42:19 -0500

    should not need config anymore cause defined in core

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,8 +13,6 @@ require "transam_cpt"
 
 module Dummy
   class Application < Rails::Application
-    config.active_record.belongs_to_required_by_default = false
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
