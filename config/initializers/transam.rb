9b7f77c75f15029ccb643433f0fedcda9cd59642
mathmerized <mathmerized@gmail.com>
2019-01-31 11:56:09 -0500

    TTPLAT-675 dont audit underway assets

--- config/initializers/transam.rb
+++ config/initializers/transam.rb
@@ -1,1 +1,3 @@
 Rails.application.config.rails_admin_cpt_lookup_tables = ['CapitalProjectType', 'MilestoneType']
+
+Rails.application.config.asset_auditor_config = {class_name: 'TransitAsset', query: {fta_asset_category: FtaAssetCategory.where.not(name: 'Infrastructure').ids, replacement_status_type_id: ReplacementStatusType.where.not(name: 'Underway').ids + [nil]}} if ActiveRecord::Base.connection.table_exists?(:fta_asset_categories)
