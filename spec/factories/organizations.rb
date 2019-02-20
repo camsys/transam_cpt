0d4a14e536feb06d605cb9a2ee496c1016fd9026
Alex Andrade <aandrade@camsys.com>
2019-02-20 15:42:14 -0500

    Surround factory values with blocks to get tests passing.

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
 
   factory :organization do
-    customer_id 1
-    address1 '100 Main St'
-    city 'Harrisburg'
-    state 'PA'
-    zip '17120'
-    url 'http://www.example.com'
-    phone '9999999999'
-    grantor_id 1
-    organization_type OrganizationType.find_by(:class_name => 'TransitOperator') rescue Rails.logger.info "ERROR: No seed data."
+    customer_id { 1 }
+    address1 { '100 Main St' }
+    city { 'Harrisburg' }
+    state { 'PA' }
+    zip { '17120' }
+    url { 'http://www.example.com' }
+    phone { '9999999999' }
+    grantor_id { 1 }
+    organization_type { OrganizationType.find_by(:class_name => 'TransitOperator') rescue Rails.logger.info "ERROR: No seed data." }
     sequence(:name) { |n| "Org #{n}" }
     short_name {name}
-    license_holder true
+    license_holder { true }
   end
 
 end
--- spec/factories/user_activity_line_item_filters.rb
+++ spec/factories/user_activity_line_item_filters.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user_activity_line_item_filter do
     sequence(:name) {|n| "Test Filter #{n}"}
-    description "Test Filter Description"
-    sogr_type "All"
-    created_by_user_id 1
+    description { "Test Filter Description" }
+    sogr_type { "All" }
+    created_by_user_id { 1 }
   end
 end
