95660e75486732182d29115c5940f72b05faa65a
mathmerized <mathmerized@gmail.com>
2019-03-03 21:00:48 -0500

    fix rails.application.config for if no DB exists

--- config/initializers/transam.rb
+++ config/initializers/transam.rb
@@ -1,3 +1,7 @@
 Rails.application.config.rails_admin_cpt_lookup_tables = ['CapitalProjectType', 'MilestoneType']
 
-Rails.application.config.asset_auditor_config = {class_name: 'TransitAsset', query: {fta_asset_category: FtaAssetCategory.where.not(name: 'Infrastructure').ids, replacement_status_type_id: ReplacementStatusType.where.not(name: 'Underway').ids + [nil]}} if ActiveRecord::Base.connection.table_exists?(:fta_asset_categories)
+begin
+  Rails.application.config.asset_auditor_config = {class_name: 'TransitAsset', query: {fta_asset_category: FtaAssetCategory.where.not(name: 'Infrastructure').ids, replacement_status_type_id: ReplacementStatusType.where.not(name: 'Underway').ids + [nil]}}
+rescue ActiveRecord::NoDatabaseError
+  puts "no database so not loading Rails.application.config that depends on DB"
+end
