36d451c533c0668b38bb8bdda631eb529b269a39
Alex Andrade <aandrade@camsys.com>
2020-01-08 15:51:07 -0500

    Update individual asset table view scripts to drop old views/table names to remain consistent with unified scripts.

--- db/events/capital_equipment_event_creation_script.sql
+++ db/events/capital_equipment_event_creation_script.sql
@@ -1,6 +1,11 @@
 -- DEVS NEED TO MAKE SURTE THEIR GLOBAL EVENT SCHEDULER IS ON
 -- SET GLOBAL event_scheduler = ON;
 
+-- IF running on a new instance that might have these views drop the views
+DROP VIEW if EXISTS capital_equipment_asset_table_views;
+DROP VIEW IF EXISTS temp_capital_equipment_asset_table_views;
+DROP TABLE IF EXISTS temp_capital_equipment_asset_table_views;
+
 CREATE TABLE IF NOT EXISTS capital_equipment_asset_table_views SELECT id FROM revenue_vehicles;
 
 
