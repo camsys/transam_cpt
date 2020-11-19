0f5a44bbd13273073dd90a970efd9d97a9c54ce8
mathmerized <mathmerized@gmail.com>
2020-11-18 19:37:10 -0500

    TTPLAT-1832 comment deprecated or not used code

--- app/searches/funding_source_searcher.rb
+++ app/searches/funding_source_searcher.rb
@@ -1,4 +1,9 @@
-# Inventory searcher. 
+# --------------------------------
+# # NOT USED see TTPLAT-1832 or https://wiki.camsys.com/pages/viewpage.action?pageId=51183790
+# --------------------------------
+
+
+# Inventory searcher.
 # Designed to be populated from a search form using a new/create controller model.
 #
 class FundingSourceSearcher < BaseSearcher
