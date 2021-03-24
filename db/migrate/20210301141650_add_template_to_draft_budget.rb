1f1a71a0e2d5159a5ff30a218f84d0099a9f94a6
Derek Edwards <dedwards8@gmail.com>
2021-03-24 11:29:40 -0400

    Don't specify int size on funding_template_id column on draft_budgets table.

--- db/migrate/20210301141650_add_template_to_draft_budget.rb
+++ db/migrate/20210301141650_add_template_to_draft_budget.rb
@@ -1,5 +1,4 @@
 class AddTemplateToDraftBudget < ActiveRecord::Migration[5.2]
   def change
-      add_reference :draft_budgets, :funding_template, foreign_key: true, type: :integer
-  end
+      add_reference :draft_budgets, :funding_template, foreign_key: true
 end
