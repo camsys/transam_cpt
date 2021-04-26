d6e8e5c736eeafac924bca00df4274220786d2b3
Derek Edwards <dedwards8@gmail.com>
2021-04-26 17:39:11 -0400

    Add link to phase assets

---
+++ app/controllers/draft_project_phase_assets_controller.rb
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Draft Budgets Controller
+#
+# Basic Draft ProjectCRUD management
+#
+#-------------------------------------------------------------------------------
+class DraftProjectPhaseAssetsController < OrganizationAwareController
+
+  add_breadcrumb "Home", :root_path
+
+  def show
+    set_scenario
+    puts @scenario.id
+    set_transit_asset
+    puts @transit_asset.id
+    set_draft_project_phase_asset
+    puts @draft_project_phase_asset 
+    puts '--------'
+    #add_breadcrumb "#{@draft_budget.name}"
+    @phases = @scenario.draft_project_phases.map{ |p| [p.name, p.object_key] }
+
+    respond_to do |format|
+      format.html
+    end
+    
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def form_params
+    #params.require(:draft_budget).permit(DraftBudget.allowable_params)
+  end
+
+  def set_scenario
+    @scenario = Scenario.find_by(object_key: params[:scenario_id])
+  end
+
+  def set_transit_asset
+    @transit_asset = TransitAsset.find_by(object_key: params[:id])
+  end
+
+  def set_draft_project_phase_asset
+    @draft_project_phase_asset = @scenario.draft_project_phase_assets.where(transit_asset: @transit_asset).first
+  end
+
+end
--- app/models/draft_project_phase_asset.rb
+++ app/models/draft_project_phase_asset.rb
@@ -1,4 +1,11 @@
 class DraftProjectPhaseAsset < ApplicationRecord
+ 
+  # Include the object key mixin
+  include TransamObjectKey
+
   belongs_to :draft_project_phase
   belongs_to :transit_asset
+
+  has_one :draft_project, through: :draft_project_phase 
+  has_one :scenario, through: :draft_project
 end
