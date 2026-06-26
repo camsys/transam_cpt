5900b13cc950036fd679f8706b1c2a6b66d513a7
aandrade <aandrade@camsys.com>
2026-06-26 12:58:23 -0400

    [TTPLAT-2730] Redirect user to new ALI page after creating a new draft project

--- app/controllers/draft_projects_controller.rb
+++ app/controllers/draft_projects_controller.rb
@@ -85,7 +85,7 @@ class DraftProjectsController < OrganizationAwareController
     respond_to do |format|
       if @draft_project.update(form_params)
         add_districts
-        format.html { redirect_to draft_project_path(@draft_project) }
+        format.html { redirect_to new_draft_project_phase_path(draft_project_id: @draft_project.object_key) }
       else
         notify_user(:alert, @draft_project.errors.full_messages.join("; "))
         format.html {redirect_back(fallback_location: root_path)}
